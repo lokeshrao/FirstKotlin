91b412fcd1f6a95779344f3c22aeef60036bb426
divyanshu <divyanshu@jnarkrd.com>
2020-04-19 22:54:24 +0530

    piddi activity layout - added list view with add button and long click delete

--- app/src/main/java/com/lk/firstkotlin/PiddiActivity.kt
+++ app/src/main/java/com/lk/firstkotlin/PiddiActivity.kt
@@ -3,34 +3,54 @@ package com.lk.firstkotlin
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_piddi.*
 
 class PiddiActivity : AppCompatActivity() {
 
-    private val listItem =
-        arrayOf("Lokesh", "Keshu", "Guddu", "PubG-player", "Rao", "its_lokesh_rao")
+     var listItem =
+         arrayListOf<String>("Lokesh", "Keshu", "Guddu", "PubG-player", "Rao", "its_lokesh_rao")
+    lateinit var addItem : Button
+    lateinit var addItemBox : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_piddi)
 
         val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItem)
+        addItem = findViewById(R.id.button)
+        addItemBox = findViewById(R.id.editText)
 
         listView.adapter = arrayAdapter //ask lokesh why different ids
-        listView.onItemClickListener =
-            AdapterView.OnItemClickListener { adapterView, view, position, id ->
+        listView.setOnItemClickListener { adapterView, view, position: Int, id: Long ->
+            val selectedItem = adapterView.getItemAtPosition(position) as String
+            val itemIdPosition = adapterView.getItemIdAtPosition(position + 1)
+            Toast.makeText(
+                applicationContext,
+                "The name you clicked is $selectedItem its position  is  $itemIdPosition",
+                Toast.LENGTH_LONG).show()
+        }
+        listView.setOnItemLongClickListener { adapterView, view, position: Int, id: Long ->
+            val selectedItem = adapterView.getItemAtPosition(position) as String
+            val itemIdPosition = adapterView.getItemIdAtPosition(position + 1)
+            Toast.makeText(
+                applicationContext,
+                "The name you clicked is $selectedItem its position  is  $itemIdPosition",
+                Toast.LENGTH_LONG).show()
+            listItem.remove(selectedItem)
+            arrayAdapter.notifyDataSetChanged()
+            return@setOnItemLongClickListener true
+        }
 
-                val selectedItem = adapterView.getItemAtPosition(position) as String
-                val itemIdPosition = adapterView.getItemIdAtPosition(position + 1)
-                Toast.makeText(
-                    applicationContext,
-                    "The name you clicked is $selectedItem its position  is  $itemIdPosition",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
+        addItem.setOnClickListener(View.OnClickListener {
+
+             listItem.add(addItemBox.text.toString())
+            addItemBox.text = null
+            arrayAdapter.notifyDataSetChanged()
+
+
+        })
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             // Apply activity transition
@@ -47,5 +67,9 @@ class PiddiActivity : AppCompatActivity() {
         onBackPressed()
         return true
     }
+
+}
+
+private fun <E> List<E>.add(e: E) {
 
 }
