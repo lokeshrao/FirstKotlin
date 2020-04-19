ec3c7851e966d7ecd981126dc4156fcb23058f96
divyanshu <divyanshu@jnarkrd.com>
2020-04-19 16:17:24 +0530

    piddi activity layout - added list view

--- app/src/main/java/com/lk/firstkotlin/MainActivity.kt
+++ app/src/main/java/com/lk/firstkotlin/MainActivity.kt
@@ -5,12 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     lateinit var piddiBtn: Button
     lateinit var keshuBtn: Button
-
-
+    var abc : Int = 0
+    lateinit var hello : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +22,14 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this@MainActivity, PiddiActivity::class.java))
         })
         keshuBtn.setOnClickListener(View.OnClickListener {
+            keshuBtn.text = "piddi"
             startActivity(Intent(this@MainActivity, KeshuActivity::class.java))
+            test()
         })
+
+    }
+    fun test(){
+        Toast.makeText(this,"hello"+piddiBtn.text,Toast.LENGTH_LONG).show()
 
     }
 }
--- app/src/main/java/com/lk/firstkotlin/PiddiActivity.kt
+++ app/src/main/java/com/lk/firstkotlin/PiddiActivity.kt
@@ -3,13 +3,35 @@ package com.lk.firstkotlin
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.lk.firstkotlin.R
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_piddi.*
 
 class PiddiActivity : AppCompatActivity() {
+
+    private val listItem =
+        arrayOf("Lokesh", "Keshu", "Guddu", "PubG-player", "Rao", "its_lokesh_rao")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_piddi)
+
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItem)
+
+        listView.adapter = arrayAdapter //ask lokesh why different ids
+        listView.onItemClickListener =
+            AdapterView.OnItemClickListener { adapterView, view, position, id ->
+
+                val selectedItem = adapterView.getItemAtPosition(position) as String
+                val itemIdPosition = adapterView.getItemIdAtPosition(position + 1)
+                Toast.makeText(
+                    applicationContext,
+                    "The name you clicked is $selectedItem its position  is  $itemIdPosition",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             // Apply activity transition
         } else {
@@ -20,6 +42,7 @@ class PiddiActivity : AppCompatActivity() {
         actionbar.setDisplayHomeAsUpEnabled(true)
 
     }
+
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
